c94eea4a4c838bb23cfdbdf2d8089b963fd63594
Dobbymin <gmin0701@knu.ac.kr>
2024-01-19 15:15:17 +0900

    [Chore]: Tailwind 설치 및 기초 세팅

--- src/components/template/HomeTemplate.jsx
+++ src/components/template/HomeTemplate.jsx
@@ -1,5 +1,9 @@
 const HomeTemplate = () => {
-    return <>HomeTemplate</>;
+    return (
+        <>
+            <div className="flex h-20 text-lg justify-center items-center">All rights reserved.</div>
+        </>
+    );
 };
 
 export default HomeTemplate;
