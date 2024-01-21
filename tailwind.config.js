0f86be79588e5161a4e232598913dd149de289ae
Dobbymin <gmin0701@knu.ac.kr>
2024-01-21 11:55:32 +0900

    [Feat]: 커스텀 색상 2개 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
+
+const colors = require('tailwindcss/colors');
+
 export default {
-    content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-    theme: {
-        extend: {},
-    },
-    plugins: [],
+	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+	theme: {
+		extend: {
+			colors: {
+				'beg-1': '#FFEDDC',
+				'gry-1': '#626161',
+			},
+		},
+	},
+	plugins: [],
 };
