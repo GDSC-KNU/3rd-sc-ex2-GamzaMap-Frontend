4530cd2553bb916ea6091ce43401ffff46346936
kimyeachan <ghaeun1009@gmail.com>
2024-01-26 16:17:06 +0900

    [Feat]: 마이 페이지 디자인

--- src/components/template/InfoTemplate.jsx
+++ src/components/template/InfoTemplate.jsx
@@ -1,5 +1,13 @@
 const InfoTemplate = () => {
-	return <>InfoTemplate</>;
+	return (
+		<section className="bg-beg-1">
+			<div className="max-w-[350px] mx-auto h-96 rounded-[20px] bg-slate-200">
+				<div>이미지</div>
+				<div>닉네임</div>
+				<div>아이디</div>
+			</div>
+		</section>
+	);
 };
 
 export default InfoTemplate;
