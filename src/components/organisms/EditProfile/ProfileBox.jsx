4b702a6c0edb91f074ae64fa5862d14ee2131254
Yea-chan Kim <ghaeun1009@gmail.com>
2024-01-27 23:52:56 +0900

    [Feat]: 사장님 인증 페이지 디자인

--- src/components/organisms/EditProfile/ProfileBox.jsx
+++ src/components/organisms/EditProfile/ProfileBox.jsx
@@ -2,7 +2,7 @@ const ProfileBox = ({ children }) => {
 	return (
 		<div
 			id="infoBox"
-			className="max-w-[350px] mx-auto h-96 rounded-[20px] bg-white p-6 relative border border-neutral-500 "
+			className="max-w-[350px] mx-auto h-96 rounded-[20px] bg-white p-8 relative border border-neutral-500 "
 		>
 			{children}
 		</div>
