735dede8f72ca27f9f547390cfcf644debbc43e8
Yea-chan Kim <ghaeun1009@gmail.com>
2024-02-11 13:41:00 +0900

    [Feat]: 사이드바 만들기

--- src/components/organisms/layout/Sidebar.jsx
+++ src/components/organisms/layout/Sidebar.jsx
@@ -1,5 +1,23 @@
 const Sidebar = () => {
-	return <>Sidebar</>;
+	return (
+		<div className="w-[58px] h-[787px] p-2.5 bg-orange-100 flex-col justify-start items-center gap-5 inline-flex">
+			<div className="py-[9px] flex-col justify-center items-center gap-[5px] flex">
+				<div className="text-neutral-500 text-[10px] font-normal font-['Jua']">
+					Pick List
+				</div>
+			</div>
+			<div className="py-[9px] flex-col justify-center items-center gap-[5px] flex">
+				<div className="text-neutral-500 text-[10px] font-normal font-['Jua']">
+					Map
+				</div>
+			</div>
+			<div className="py-[9px] flex-col justify-center items-center gap-[5px] flex">
+				<div className="text-neutral-500 text-[10px] font-normal font-['Jua']">
+					Chatting
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Sidebar;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,13 @@
 import HomeTemplate from '../components/template/HomeTemplate';
-
+import Sidebar from '../components/organisms/layout/Sidebar';
 const HomePage = () => {
 	return (
 		<>
 			<HomeTemplate />
+			<div>
+				<Sidebar />
+				<div>home</div>
+			</div>
 		</>
 	);
 };
