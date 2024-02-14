3626d9c0e55e63a1be24828f4cfa6dceb098e3f7
Dobbymin <gmin0701@knu.ac.kr>
2024-02-15 01:20:00 +0900

    [Feat]: Sidebar 구현

--- src/components/organisms/layout/Sidebar.jsx
+++ src/components/organisms/layout/Sidebar.jsx
@@ -1,5 +1,15 @@
+import { SidebarContainer } from '../../../styles/SidebarStyle';
+
+import Buttons from '../Sidebar/Buttons';
+
 const Sidebar = () => {
-	return <>Sidebar</>;
+	return (
+		<>
+			<SidebarContainer>
+				<Buttons />
+			</SidebarContainer>
+		</>
+	);
 };
 
 export default Sidebar;
