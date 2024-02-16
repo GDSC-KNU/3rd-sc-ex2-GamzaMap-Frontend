29c5ed6f4a73e51b1aa8bba8ac29ff8ab5679b9a
Dobbymin <gmin0701@knu.ac.kr>
2024-02-16 16:56:08 +0900

    [Feat]: Header 코드 컴포넌트화

--- src/components/common/Button/LoginButton.jsx
+++ src/components/common/Button/LoginButton.jsx
@@ -1,5 +1,35 @@
+import { useState } from 'react';
+
+import LoginBtn from '../../../assets/Header/LoginButton.svg';
+import UserImg from '../../../assets/Header/UserProfileImg.svg';
+import {
+	ProfileButtonStyle,
+	ProfileContainer,
+} from '../../../styles/HeaderStyle';
+
 const LoginButton = () => {
-	return <>LoginButton</>;
+	const [imgSrc, setImgSrc] = useState(LoginBtn);
+	const [isClicked, setIsClicked] = useState(false);
+
+	const onClickHandler = () => {
+		if (isClicked) {
+			setImgSrc(UserImg);
+			setIsClicked(false);
+		} else {
+			setImgSrc(LoginBtn);
+			setIsClicked(true);
+		}
+	};
+	return (
+		<>
+			<ProfileContainer id="ProfileContainer">
+				<ProfileButtonStyle>
+					<img onClick={onClickHandler} src={imgSrc} />
+				</ProfileButtonStyle>
+				{/* <ProfileButton /> */}
+			</ProfileContainer>
+		</>
+	);
 };
 
 export default LoginButton;
--- src/components/common/Input/SearchInput.jsx
+++ src/components/common/Input/SearchInput.jsx
@@ -1,12 +1,14 @@
+import { SearchInputContainer } from '../../../styles/HeaderStyle';
+
 const SearchInput = () => {
 	return (
 		<>
-			<input
-				type="search"
+			<SearchInputContainer
+				// type="search"
 				id="searchBar"
-				className="block w-full p-4 pe-10 text-lg text-gray-900 border border-gry-1 rounded-xl"
-				placeholder="장소를 검색해주세요"
-				required
+				// className="block w-full p-4 pe-10 text-lg text-gray-900 border border-gry-1 rounded-xl"
+				placeholder="Please search for a place"
+				// required
 			/>
 		</>
 	);
