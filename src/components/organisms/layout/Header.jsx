c88497d11a8304a660f1d030f46f66baf38c5e1b
Dobbymin <gmin0701@knu.ac.kr>
2024-02-16 17:03:50 +0900

    [Feat]: Header 구현 완료

--- src/components/organisms/layout/Header.jsx
+++ src/components/organisms/layout/Header.jsx
@@ -2,32 +2,33 @@ import { IoIosSearch } from 'react-icons/io';
 
 import HeaderButtons from '../../common/Button/HeaderButtons';
 import LogoButton from '../../common/Button/LogoButton';
-import ProfileButton from '../../common/Button/ProfileButton';
 import SearchInput from '../../common/Input/SearchInput';
+import LoginButton from '../../common/Button/LoginButton';
+import {
+	HeaderButtonContainer,
+	HeaderContainer,
+	InputContainer,
+	LeftContainer,
+} from '../../../styles/HeaderStyle';
 
 const Header = () => {
 	return (
-		<header className="w-full h-20 bg-beg-1">
-			<div className="flex gap-2 h-full justify-center items-center">
-				<LogoButton logoName="GamzaMap" />
-				<form className="basis-1/2">
-					<div className="relative">
-						<div className="absolute inset-y-0 end-3 flex items-center cursor-pointer">
-							<IoIosSearch className="text-3xl" />
-						</div>
-						<SearchInput />
-					</div>
-				</form>
-				<div id="locationSection" className="basis-2/3 flex gap-6 mx-4">
-					<HeaderButtons icon="cafe" text="카페" />
-					<HeaderButtons icon="restaurant" text="식당" />
-					<HeaderButtons icon="gas" text="주유소" />
-				</div>
-				<div id="profileSection" className="basis-40">
-					<ProfileButton />
-				</div>
-			</div>
-		</header>
+		<HeaderContainer id="HeaderContainer">
+			<LeftContainer>
+				<LogoButton id="LogoButton" logoName="GamzaMap" />
+				<InputContainer>
+					<SearchInput />
+					<IoIosSearch className="text-2xl text-stone-500" />
+				</InputContainer>
+
+				<HeaderButtonContainer id="locationSection">
+					<HeaderButtons icon="cafe" text="Cafe" />
+					<HeaderButtons icon="restaurant" text="Restaurant" />
+					<HeaderButtons icon="cvs" text="CVS" />
+				</HeaderButtonContainer>
+			</LeftContainer>
+			<LoginButton />
+		</HeaderContainer>
 	);
 };
 
