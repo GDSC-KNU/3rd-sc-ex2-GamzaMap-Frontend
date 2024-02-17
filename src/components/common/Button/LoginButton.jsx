fb527843338b66bcf13d253db35a037d267b63fd
Dobbymin <gmin0701@knu.ac.kr>
2024-02-17 21:07:11 +0900

    [Feat]: 로그인 페이지 이동 기능 추가

--- src/components/common/Button/LoginButton.jsx
+++ src/components/common/Button/LoginButton.jsx
@@ -1,35 +1,39 @@
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import LoginBtn from '../../../assets/Header/LoginButton.svg';
 import UserImg from '../../../assets/Header/UserProfileImg.svg';
 import {
-	ProfileButtonStyle,
-	ProfileContainer,
+    ProfileButtonStyle,
+    ProfileContainer,
 } from '../../../styles/HeaderStyle';
 
 const LoginButton = () => {
-	const [imgSrc, setImgSrc] = useState(LoginBtn);
-	const [isClicked, setIsClicked] = useState(false);
+    const [imgSrc, setImgSrc] = useState(LoginBtn);
+    const [isClicked, setIsClicked] = useState(false);
 
-	const onClickHandler = () => {
-		if (isClicked) {
-			setImgSrc(UserImg);
-			setIsClicked(false);
-		} else {
-			setImgSrc(LoginBtn);
-			setIsClicked(true);
-		}
-	};
-	return (
-		<>
-			<ProfileContainer id="ProfileContainer">
-				<ProfileButtonStyle>
-					<img onClick={onClickHandler} src={imgSrc} />
-				</ProfileButtonStyle>
-				{/* <ProfileButton /> */}
-			</ProfileContainer>
-		</>
-	);
+    const navigate = useNavigate();
+
+    const onClickHandler = () => {
+        if (isClicked) {
+            setImgSrc(UserImg);
+            setIsClicked(false);
+            navigate('/auth/login');
+        } else {
+            setImgSrc(LoginBtn);
+            setIsClicked(true);
+        }
+    };
+    return (
+        <>
+            <ProfileContainer id="ProfileContainer">
+                <ProfileButtonStyle>
+                    <img onClick={onClickHandler} src={imgSrc} />
+                </ProfileButtonStyle>
+                {/* <ProfileButton /> */}
+            </ProfileContainer>
+        </>
+    );
 };
 
 export default LoginButton;
