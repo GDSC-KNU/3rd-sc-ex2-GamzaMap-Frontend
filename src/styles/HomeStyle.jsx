c767f34b7d5628d7b500526ecedf57bb791bff4f
Dobbymin <gmin0701@knu.ac.kr>
2024-02-17 15:45:15 +0900

    [Feat]: HomePage 구현 및 컴포넌트화

---
+++ src/styles/HomeStyle.jsx
@@ -0,0 +1,57 @@
+import styled from 'styled-components';
+
+export const HomeContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 20px;
+`;
+export const TextContainer01 = styled.div`
+    display: flex;
+    /* height: 109px; */
+    justify-content: center;
+    align-items: center;
+    gap: 20px;
+    margin-top: 3rem;
+    margin-right: 6rem;
+`;
+
+export const TextContainer02 = styled.div`
+    display: flex;
+    align-items: center;
+    gap: 10px;
+`;
+
+export const ArrowContainer = styled.div`
+    width: 4.8rem;
+    height: 4.8rem;
+`;
+
+export const TextItem01 = styled.div`
+    color: #000;
+    font-family: Jua;
+    font-size: 2rem;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+`;
+
+export const TextItem02 = styled.div`
+    color: #000;
+    font-family: Jua;
+    font-size: 2rem;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+`;
+
+export const PotatoImgContainer = styled.div`
+    /* width: 70px; */
+    /* height: 70px; */
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    width: 10rem;
+    height: 10rem;
+`;
