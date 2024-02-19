1e8eae93e7c6f8e77199f2fa9a98032f446b8acb
Dobbymin <gmin0701@knu.ac.kr>
2024-02-19 09:35:34 +0900

    [Fix]: Router 설정 변경

--- src/utils/router.jsx
+++ src/utils/router.jsx
@@ -9,46 +9,53 @@ import InfoPage from '../pages/Info';
 import CheckBossPage from '../pages/CheckBossPage';
 import SearchPage from '../pages/Search';
 import PickPage from '../pages/pick';
-import DetailPage from '../pages/Detail';
+// import DetailPage from '../pages/Detail';
 import MapPage from '../pages/Map';
+import StoreDetailPage from '../pages/StoreDetail';
 
 const router = createBrowserRouter([
-	{ index: true, path: '/', element: <HomePage /> },
-	{
-		path: 'auth',
-		children: [
-			{
-				path: 'login',
-				element: <LoginPage />,
-			},
-			{
-				path: 'register',
-				element: <RegisterPage />,
-			},
-		],
-	},
-	{ path: 'chatting', element: <ChattingPage /> },
-	{
-		path: 'mypage',
-		children: [
-			{
-				path: '',
-				element: <InfoPage />,
-			},
-			{
-				path: 'profile',
-				element: <EditProfilePage />,
-			},
-			{
-				path: 'checkboss',
-				element: <CheckBossPage />,
-			},
-		],
-	},
-	{ path: 'search', element: <SearchPage /> },
-	{ path: 'pick', element: <PickPage /> },
-	{ path: 'map', element: <MapPage /> },
-	{ path: 'detail', element: <DetailPage /> },
+    { index: true, path: '/', element: <HomePage /> },
+    {
+        path: 'auth',
+        children: [
+            {
+                path: 'login',
+                element: <LoginPage />,
+            },
+            {
+                path: 'register',
+                element: <RegisterPage />,
+            },
+        ],
+    },
+    { path: 'chatting', element: <ChattingPage /> },
+    {
+        path: 'mypage',
+        children: [
+            {
+                path: '',
+                element: <InfoPage />,
+            },
+            {
+                path: 'profile',
+                element: <EditProfilePage />,
+            },
+            {
+                path: 'checkboss',
+                element: <CheckBossPage />,
+            },
+        ],
+    },
+    { path: 'search', element: <SearchPage /> },
+    { path: 'pick', element: <PickPage /> },
+    {
+        path: 'map',
+        children: [
+            { path: '', element: <MapPage /> },
+            { path: 'detail', element: <StoreDetailPage /> },
+        ],
+    },
+    // { path: 'detail', element: <DetailPage /> },
 ]);
 
 export default router;
