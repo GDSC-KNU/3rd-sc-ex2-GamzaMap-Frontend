919d5b01235905193437c7a64119aa531e50bf24
Dobbymin <gmin0701@knu.ac.kr>
2024-02-22 00:35:13 +0900

    [Chore]: google-map api 간략화

---
+++ src/utils/URL.jsx
@@ -0,0 +1,3 @@
+export const BASE_URL = import.meta.env.VITE_APP_BASE_URL;
+export const REST_API_KEY = import.meta.env.VITE_APP_REST_API_KEY;
+export const MAP_API_KEY = import.meta.env.VITE_APP_MAP_API_KEY;
