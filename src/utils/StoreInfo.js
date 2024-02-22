03b90cc5d402018671fd47324e856701d9ced834
Dobbymin <gmin0701@knu.ac.kr>
2024-02-22 15:11:56 +0900

    [Feat]: 표시할 가게 정보 모음

---
+++ src/utils/StoreInfo.js
@@ -0,0 +1,44 @@
+export const StoreInfo = [
+    {
+        title: 'Cafe Concrete',
+        number: '010-3444-4364',
+        address01: 'Daegu Buk-gu',
+        address02: 'Sangyeok-ro 6-gil 1393-13',
+        position: { lat: 35.8864, lng: 128.6071 },
+    },
+    {
+        title: 'Keanu',
+        number: '053-952-7674',
+        address01: 'Daegu Buk-gu',
+        address02: 'Daehyeon-ro 19-gil, 38',
+        position: { lat: 35.8859, lng: 128.6101 },
+    },
+    {
+        title: 'W Collection',
+        number: '053-944-2380',
+        address01: 'Daegu Buk-gu',
+        address02: 'Gyeongdae-ro, 31',
+        position: { lat: 35.884466, lng: 128.613471 },
+    },
+    {
+        title: 'Baek So-jeong',
+        number: '053-939-8862',
+        address01: 'Daegu Buk-gu',
+        address02: 'Daehak-ro, 89-1',
+        position: { lat: 35.893, lng: 128.6099 },
+    },
+    {
+        title: 'Goni Table',
+        number: '053-939-5585',
+        address01: 'Daegu Buk-gu',
+        address02: 'Sangyeok-ro 6-gil, 29',
+        position: { lat: 35.8926, lng: 128.6085 },
+    },
+    {
+        title: 'Myeon-Tamjeong',
+        number: '053-215-8386',
+        address01: 'Daegu Buk-gu',
+        address02: 'Sangyeok-ro 8-gil, 36',
+        position: { lat: 35.893555, lng: 128.609149 },
+    },
+];
