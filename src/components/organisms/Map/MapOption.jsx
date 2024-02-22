c33fd64d118237d2eba3c45deb487efdb5e16f50
Dobbymin <gmin0701@knu.ac.kr>
2024-02-22 15:12:15 +0900

    [Feat]: 가게 정보 적용 및 Map 스타일 적용

---
+++ src/components/organisms/Map/MapOption.jsx
@@ -0,0 +1,40 @@
+import { StoreInfo } from '../../../utils/StoreInfo';
+
+export const markers = StoreInfo.map((store) => ({
+    title: store.title,
+    number: store.number,
+    position: store.position,
+}));
+
+export const styles = [
+    {
+        featureType: 'road',
+        elementType: 'labels',
+        stylers: [{ visibility: 'on' }],
+    },
+    {
+        featureType: 'poi',
+        elementType: 'labels',
+        stylers: [{ visibility: 'off' }],
+    },
+    {
+        featureType: 'transit',
+        elementType: 'labels',
+        stylers: [{ visibility: 'on' }],
+    },
+    {
+        featureType: 'landscape',
+        elementType: 'labels',
+        stylers: [{ visibility: 'on' }],
+    },
+    {
+        featureType: 'administrative',
+        elementType: 'labels',
+        stylers: [{ visibility: 'on' }],
+    },
+    {
+        featureType: 'water',
+        elementType: 'labels',
+        stylers: [{ visibility: 'off' }],
+    },
+];
